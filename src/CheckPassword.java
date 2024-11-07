97b3487217cea3abcdb5e3dc89a5e9b0774f93a3
harii77 <hariharanvj7777@gmail.com>
2024-11-07 18:33:40 +0530

    day 3

--- src/CheckPassword.java
+++ src/CheckPassword.java
@@ -42,7 +42,7 @@ public class CheckPassword {
         if(str.charAt(0) >= '0'  &&  str.charAt(0) <= '9') return 0;
 
         for (int i = 0; i < str.length(); i++) {
-            if(str.length() <= 4)return 0;
+            if(str.length() <= 4)  return 0;
             if(str.charAt(i) >= '0'  &&  str.charAt(i) <= '9')  numericDigit++;
             if(str.charAt(i) >= 'A'  &&  str.charAt(i) <= 'Z')  capitalLetters++;
             if(str.charAt(i) == ' '  &&  str.charAt(i) == '/')  return 0;
