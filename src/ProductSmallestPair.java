f79c713ca5aa7581849d1c2eca2a7532d21d89f3
harii77 <hariharanvj7777@gmail.com>
2024-11-13 12:54:38 +0530

    day 9

---
+++ src/ProductSmallestPair.java
@@ -0,0 +1,38 @@
+/*
+    Problem Statement:
+
+    Write a function int productOfSmallestPair(int[] arr) that takes an integer array arr as input and returns the product
+    of the two smallest distinct elements in the array.
+
+    Input:
+        An integer array arr of size n, where n >= 2.
+    Output:
+        Return the product of the two smallest distinct elements in arr.
+
+        If the array has fewer than two distinct elements, return -1.
+
+    Function Requirements:
+        Your function should first identify the two smallest distinct elements in the array.
+        Then, it should compute and return the product of these two elements.
+
+    Example:
+    Input:
+        arr = [5, 2, 8, 1, 9]
+    Output:
+        2
+    Explanation:
+        In the array [5, 2, 8, 1, 9], the two smallest distinct elements are 1 and 2. Their product is 1 * 2 = 2.
+
+
+    Edge Case Example:
+    Input:
+        arr = [5, 5, 5]
+    Output:
+        -1
+    Explanation:
+        Since there are not enough distinct elements in the array, the function returns -1.
+*/
+
+public class ProductSmallestPair {
+
+}
