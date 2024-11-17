d2dc3ab6af578461bc447bb02d29ce469f17440e
harii77 <hariharanvj7777@gmail.com>
2024-11-17 18:00:02 +0530

    day 12

---
+++ src/Prime.java
@@ -0,0 +1,13 @@
+public class Prime {
+    public static void main(String[] args) {
+        int inputNum1 = 7;
+        int inputNum2 = 5;
+        System.out.println(isPrime(inputNum1));
+        System.out.println(isPrime(inputNum2));
+    }
+
+    static boolean isPrime(int num){
+        for (int i = 2; i*i < num; i++) if(num % i == 0) return false;
+        return true;
+    }
+}
